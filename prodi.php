fa3aee29c38af520b4bde782d4984baba19d01ab
zikrihasbi <jackhsb22@gmail.com>
2025-03-10 13:48:00 +0800

    formula

---
+++ prodi.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIMPADU POLIBAN</title>
+</head>
+<body>
+    <h1>DATA PRODI</h1>
+    <br>
+    <table border ="1">
+        <thead>
+            <tr>
+            <th>No</th>
+            <th>NIM</th>
+            <th>Nama</th>
+            </tr>
+        <thead>
+    <tbody>
+            <tr>        
+                <td>1</td>
+                <td>E020323040</td>
+                <td>Muhammad Zikri Hasbi</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>E020323081</td>
+                <td>Jamal</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>E020323001</td>
+                <td>Malik</td>
+            </tr>
+            <tr>
+</body>
+</html>
